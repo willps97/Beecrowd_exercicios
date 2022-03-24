31ce30c150d796adcf60b8eacd0e78915556deb3
William Pereira dos Santos <william.sanctos@gmail.com>
2022-03-23 21:30:43 -0300

    Exercicios feitos em 23/03/2022.

---
+++ Beecrowd_1047/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
+Obs: O jogo tem duração mínima de um (1) minuto e duração máxima de 24 horas.*/
+
+namespace Beecrowd_1047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada = Console.ReadLine();
+            string[] data = entrada.Split(' ');
+            int horaInicial = int.Parse(data[0]);
+            int minutoInicial = int.Parse(data[1]);
+            int horaFinal = int.Parse(data[2]);
+            int minutoFinal = int.Parse(data[3]);
+
+            minutoInicial += horaInicial * 60;
+            minutoFinal += horaFinal * 60;
+            int tempo = minutoFinal - minutoInicial;
+
+            if (tempo <= 0)
+            {
+                tempo += 24 * 60;
+            }
+
+            int h = tempo / 60;
+            int m = tempo % 60;
+
+            Console.WriteLine($"O JOGO DUROU {h} HORA(S) E {m} MINUTO(S)");
+            
+        }
+    }
+}
+
---
+++ Beecrowd_1048/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+/* A empresa ABC resolveu conceder um aumento de salários a seus funcionários de acordo com a tabela abaixo:
+ 0 - 400 = 15%
+400.01 - 800.00 = 12%
+800.01 - 1200.00 = 10%
+1200.01 - 2000.00 = 7%
+Acima de 2000.00 = 4%
+*/
+
+namespace Beecrowd_1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            double ganho = 0;
+            double percentual = 0;
+            double salarioFinal = 0;
+            
+            if (salario >= 0.00 && salario <= 400.00)
+            {
+                percentual = 15.00;
+                ganho = salario * (percentual / 100);
+                salarioFinal = salario + ganho;
+            }
+            else if (salario >= 400.01 && salario <= 800.00)
+            {
+                percentual = 12.00;
+                ganho = salario * (percentual / 100);
+                salarioFinal = salario + ganho;
+            }
+            else if (salario >= 800.01 && salario <= 1200.00)
+            {
+                percentual = 10.00;
+                ganho = salario * (percentual / 100);
+                salarioFinal = salario + ganho;
+            }
+            else if (salario >= 1200.01 && salario <= 2000.00)
+            {
+                percentual = 7.00;
+                ganho = salario * (percentual / 100);
+                salarioFinal = salario + ganho;
+            }
+            else if (salario > 2000.00)
+            {
+                percentual = 4.00;
+                ganho = salario * (percentual / 100);
+                salarioFinal = salario + ganho;
+            }
+            Console.WriteLine("Novo salario: " + salarioFinal.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + ganho.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: " + percentual.ToString("F0",CultureInfo.InvariantCulture)+" %");
+        }
+    }
+}
